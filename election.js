7a7b45ac776ac497de9e9121313f91f558cd0bc6
COREY COSMAN <cosman.corey@gmail.com>
2017-11-16 14:46:13 -0500

    pulled api data, displayed it on page using js

--- election.js
+++ election.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var body = document.querySelector("body");
+  var myElement = document.createElement('ul');
+  var button = document.querySelector("#btn")
 
-  // Imagination!
+  button.addEventListener('click', function(){
+    $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: "GET"
+  }).done(function(data){
+      // myElement.innerHTML = data;
+      console.log(data)
+      for (i=0;i<data.candidates.length;i++){
+        var list = document.createElement("li");
+        list.innerText = data.candidates[i].name + " | " + data.candidates[i].votes  ;
+        myElement.append(list)
+      }
+      document.body.append(myElement);
+
+    })
+  })
+
+
+
 
 });
